5b58037b8b13d3c0e252c147f3a5481ca671ee93
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-09-30 14:56:02 +0300

    add variable practice js file solved

---
+++ variables-practice.js
@@ -0,0 +1,40 @@
+// The Fortune Teller
+let numberChildren = 1;
+let partnerName = "herpet";
+let geographicLocation = "mars";
+let jobTitle = "overlord";
+const fortune = "you will be a " + jobTitle + " in " + geographicLocation + " and married to " + partnerName + " with " + numberChildren + "child."
+
+console.log(fortune);
+
+
+// The Age Calculator
+let cYear = 2019;
+let bYear = 1990;
+const yourAge = "you are either " + (cYear - bYear) + " or" + (cYear - bYear - 1);
+console.log(yourAge);
+
+// The Lifetime Supply Calculator
+let cAge = 29;
+let maxAge = 100;
+let amountPerDay = 2;
+let supply = 365 * amountPerDay * (maxAge - cAge)
+const snack = "you will need " + supply + " untill the ripe age of " + maxAge;
+console.log(snack);
+
+// The Geometrizer
+let radius = 50;
+let circumferance = 
+
+
+// The Temperature Converter
+let tempC = 34;
+const cToF = ((34 *1.8)+32)
+let outCToF = tempC +  "C is " + cToF + "F";
+console.log(outCToF);
+
+
+let tempF = 74;
+const fToC = ((34 *1.8)+32);
+let outFtoC = (tempF -32) * (5/9);
+console.log(tempF + "F is " + outFtoC + "C");
